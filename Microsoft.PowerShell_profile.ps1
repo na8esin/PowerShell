f47788edd94d7d511e5fae214a6da2222aab6490
na8esin <na8esin.mpst@gmail.com>
2020-05-26 08:46:33 +0900

    コマンド追加

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,17 +1,39 @@
 Set-Alias -name gco -value gitCheckout
 Set-Alias -name gca -value gitCommitAutomaticallyStageFiles
+Set-Alias -name gcad -value gitCommitAutomaticallyStageFilesDevelop
 Set-Alias -name gpom -value gitPushOriginMaster
+Set-Alias -name gpod -value gitPushOriginDevelop
+Set-Alias -name glo -value gitLogOneline
+Set-Alias -name unit -value vendorBinPhpUnit
+Set-Alias -name cakeserver -value binCakeServer
 
 function prompt(){
     $Host.ui.RawUI.WindowTitle = Convert-Path (pwd).path
+    $env:LANG = "ja_JP.UTF-8"
 }
 
+function binCakeServer {
+  bin/cake server -H 0.0.0.0
+}
+function vendorBinPhpUnit($path) {
+  vendor/bin/phpunit $path
+}
+
+function gitCommitAutomaticallyStageFilesDevelop($message) {
+  git pull ; gca $message; gpod;
+}
 function gitCommitAutomaticallyStageFiles($message) {
   git commit -a -m $message
+}
+function gitPushOriginDevelop() {
+  git push origin develop
 }
 function gitPushOriginMaster() {
   git push origin master
 }
 function gitCheckout() {
   git checkout
+}
+function gitLogOneline($rows) {
+  git log --oneline $rows
 }
